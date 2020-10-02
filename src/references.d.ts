4627360da1c4fd4883d0f2fdc6a518758efe5146
Jeroen Bobbeldijk <jeroen@klippa.com>
2020-10-02 11:46:07 +0200

    Change some stuff for better NS7 approach

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,8 +1,8 @@
 /// <reference path="./node_modules/@nativescript/core/global-types.d.ts" />
 /// <reference path="./node_modules/@nativescript/types/index.d.ts" />
 
-/// <reference path="./platforms/ios/typings/objc!GoogleMapsBase.d.ts" />
-/// <reference path="./platforms/ios/typings/objc!GoogleMaps.d.ts" />
+/// <reference path="./typings/objc!GoogleMapsBase.d.ts" />
+/// <reference path="./typings/objc!GoogleMaps.d.ts" />
 
 /*
  To use a specific platform version definition:
